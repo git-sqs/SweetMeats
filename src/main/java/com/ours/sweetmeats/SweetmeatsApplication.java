f26efde9c9a382f902cbb5901068492c3897eb81
git-sqs <sqs497848008@163.com>
2020-01-14 12:33:26 +0800

    首次创建

---
+++ src/main/java/com/ours/sweetmeats/SweetmeatsApplication.java
@@ -0,0 +1,13 @@
+package com.ours.sweetmeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetmeatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweetmeatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ours/sweetmeats/SweetmeatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ours.sweetmeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetmeatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
