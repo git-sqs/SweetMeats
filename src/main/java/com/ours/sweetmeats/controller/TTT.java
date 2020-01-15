6cc1597c3b68e0a18b2bb110cae0d6a966799e01
Admin_ZY <Mr-zhangya>
2020-01-15 17:33:12 +0800

    测试提交111222

--- src/main/java/com/ours/sweetmeats/controller/TTT.java
+++ src/main/java/com/ours/sweetmeats/controller/TTT.java
@@ -7,4 +7,7 @@ package com.ours.sweetmeats.controller;
  * @DATE: 2020/1/15 17:28
  */
 public class TTT {
+    public static void main(String[] args) {
+        System.out.println("111111");
+    }
 }
