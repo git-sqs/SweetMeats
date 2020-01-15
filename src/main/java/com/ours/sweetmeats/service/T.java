8e12e4b3277f5691f2fcd92bca56fb513e6f8307
Admin_ZY <Mr-zhangya>
2020-01-15 17:20:32 +0800

    测试提交

---
+++ src/main/java/com/ours/sweetmeats/service/T.java
@@ -0,0 +1,10 @@
+package com.ours.sweetmeats.service;
+
+/**
+ * @PROJECT_NAME: SweetMeats
+ * @DESCRIPTION:
+ * @USER: Admin_ZY
+ * @DATE: 2020/1/15 17:19
+ */
+public class T {
+}
---
+++ src/main/java/com/ours/sweetmeats/service/impl/T.java
@@ -0,0 +1,10 @@
+package com.ours.sweetmeats.service.impl;
+
+/**
+ * @PROJECT_NAME: SweetMeats
+ * @DESCRIPTION:
+ * @USER: Admin_ZY
+ * @DATE: 2020/1/15 17:20
+ */
+public class T {
+}
